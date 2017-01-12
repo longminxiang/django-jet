f88139d870526be645f6fd66fc0952c1cde8322c
eric <longminxiang@163.com>
2017-01-12 18:02:44 +0800

    custom begin

--- jet/static/jet/js/src/features/sidebar/main.js
+++ jet/static/jet/js/src/features/sidebar/main.js
@@ -1,6 +1,4 @@
 var $ = require('jquery');
-var SideBarApplicationPinning = require('./application-pinning');
-var SideBarBookmarks = require('./bookmarks');
 var SideBarPopup = require('./popup');
 
 require('perfect-scrollbar/jquery')($);
@@ -19,6 +17,7 @@ SideBar.prototype = {
     },
     initSideBarToggle: function() {
         $('.sidebar-toggle').on('click', function(e) {
+            console.log('click');
             e.preventDefault();
 
             var $dependent = $('.sidebar-dependent');
@@ -28,16 +27,57 @@ SideBar.prototype = {
             $dependent.toggleClass('sidebar-opened', open);
         });
     },
+    initMenuToggle: function() {
+        var menu = '.sidebar';
+        var animationSpeed = 200;
+        $(document).off('click', menu + ' li a').on('click', menu + ' li a', function (e) {
+            //Get the clicked link and the next element
+            var $this = $(this);
+            var checkElement = $this.next();
+
+            //Check if the next element is a menu and is visible
+            if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible')) && (!$('body').hasClass('sidebar-collapse'))) {
+                //Close the menu
+                checkElement.slideUp(animationSpeed, function () {
+                    checkElement.removeClass('menu-open');
+                    //Fix the layout in case the sidebar stretches over the height of the window
+                });
+                checkElement.parent("li").removeClass("active");
+            }
+            //If the menu is not visible
+            else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+                //Get the parent menu
+                var parent = $this.parents('ul').first();
+                //Close all open menus within the parent
+                var ul = parent.find('ul:visible').slideUp(animationSpeed);
+                //Remove the menu-open class from the parent
+                ul.removeClass('menu-open');
+                //Get the parent li
+                var parent_li = $this.parent("li");
+
+                //Open the target menu and add the menu-open class
+                checkElement.slideDown(animationSpeed, function () {
+                    //Add the class active to the parent li
+                    checkElement.addClass('menu-open');
+                    parent.find('li.active').removeClass('active');
+                    parent_li.addClass('active');
+                });
+            }
+            //if this isn't a link, prevent the page from being redirected
+            if (checkElement.is('.treeview-menu')) {
+                e.preventDefault();
+            }
+        });
+    },
     run: function() {
         var $sidebar = this.$sidebar;
 
-        new SideBarApplicationPinning($sidebar).run();
-        new SideBarBookmarks($sidebar).run();
         new SideBarPopup($sidebar).run();
 
         try {
             this.initScrollBars($sidebar);
             this.initSideBarToggle();
+            this.initMenuToggle();
         } catch (e) {
             console.error(e, e.stack);
         }
